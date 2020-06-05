59ea6c53d86fae61f055a9e878524f4d0e57abe1
JJY <jj2915@nyu.edu>
2020-06-05 19:27:20 +0800

    Syntax adjust

--- SOFT/testArUco/cam_calib/camera_calibration.py
+++ SOFT/testArUco/cam_calib/camera_calibration.py
@@ -25,14 +25,14 @@ def cam_calib():
     h = 6
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
     # prepare cv2 world coordinate as standard base.
-    objp = np.zeros((w*h,3), np.float32)
+    objp = np.zeros((w * h, 3), np.float32)
     objp[:, :2] = np.mgrid[0:w, 0:h].T.reshape(-1, 2)
     objp = objp * 22  # height and width of chessboard is 22mm
     obj_point = []  # 3d point in real world space
     img_point = []  # 2d points in image plane.
 
     ############################ IMAGE PROCESS #####################################
-    route = os.getcwd() + "\cam_calib\*.jpg"
+    route = os.getcwd() + "/cam_calib/*.jpg"
     print(route)
     images = glob.glob(route)  # route for chessboard photos
     for pic in images:
@@ -61,29 +61,24 @@ def cam_calib():
 
 
 def cam_calib_charuco():
-        '''
+    '''
         camera calibration using CharUco boards.
         '''
-        l_corners = []
-        l_ids = []
-        marker_dict = aruco_Dictionary.get(aruco.DICT_4X4_50)
-        corners = []
+    l_corners = []
+    l_ids = []
+    marker_dict = aruco_Dictionary.get(aruco.DICT_4X4_50)
+    corners = []
+    board = aruco_CharucoBoard.create(9, 6, 200, 150, marker_dict)
+    for images in glob.glob(".\calib\*.jpg"):
+        img = cv.imread(images)
         board = aruco_CharucoBoard.create(9, 6, 200, 150, marker_dict)
-        for images in glob.glob(".\calib\*.jpg"):
-            img = cv.imread(images)
-            board = aruco_CharucoBoard.create(9, 6, 200, 150, marker_dict)
-            corners, ids, = aruco.detectMarkers(img, marker_dict, None, None)
-            if len(ids) > 0:
-                a, b = aruco.interpolateCornersCharuco(corners, ids, img,
-                                                       board, None, None)
-                l_corners.append(a)
-                l_ids.append(b)
-        return aruco.calibrateCameraCharuco(l_corners,\
-                                            l_ids,\
-                                            board,\
-                                            (600, 800),
-                                            None,\
-                                            None)
+        corners, ids, = aruco.detectMarkers(img, marker_dict, None, None)
+        if len(ids) > 0:
+            a, b = aruco.interpolateCornersCharuco(corners, ids, img,
+                                                   board, None, None)
+            l_corners.append(a)
+            l_ids.append(b)
+    return aruco.calibrateCameraCharuco(l_corners, l_ids, board, (600, 800), None, None)
 
 
 def display_calib_rst():
@@ -93,14 +88,14 @@ def display_calib_rst():
     images = glob.glob(".\calib\*.jpg")  # route for chessboard photos
     for img_route in images:
         img = cv.imread(img_route)
-        h,w = img.shape[:2]
+        h, w = img.shape[:2]
         newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
         # dst = cv.undistort(img,cam_param[1],cam_param[2],None, newcammtx)
         mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx,
                                                 (w, h), 5)
         dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
         x, y, w, h = roi
-        dst = dst[y:y+h, x:x+w]
+        dst = dst[y:y + h, x:x + w]
         # cv.namedWindow('org',0)
         # cv.namedWindow('dst',0)
         cv.imshow("org", img)
@@ -110,7 +105,7 @@ def display_calib_rst():
 
 
 if __name__ == "__main__":
-    rt, mtx, dist, rvecs, tvecs = cam_calib() #cam_calib_charuco()
+    rt, mtx, dist, rvecs, tvecs = cam_calib()  # cam_calib_charuco()
     # cam_calib_charuco()
     print(mtx)
     print(dist)
