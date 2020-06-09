ca41999dc01b42cd0a83bf54c2d38146f3c9af90
sheldonchen <nh8157@hotmail.com>
2020-06-09 16:43:24 +0800

    move fixed

--- SOFT/arduino_multicast_move_simple/arduino_multicast_move_simple.ino
+++ SOFT/arduino_multicast_move_simple/arduino_multicast_move_simple.ino
@@ -6,11 +6,7 @@
 #define DIRA 14
 #define PWMB 12
 #define DIRB 13
-#define MUXI 34
-#define MUXA 5
-#define MUXB 18
-#define MUXC 19
-#define PULSE 312
+#define PULSE 7
 
 int STATE = 0;
 int* POS;
@@ -35,6 +31,8 @@ void IRAM_ATTR isr2() {
   encoder2.tickOn = true;
 }
 
+//void turnRight(int* POS, int* );
+
 const char * ssid = "nowifi";
 const char * password = "durf2020";
 
@@ -43,10 +41,10 @@ DynamicJsonDocument jPos(1024);
 
 void setup()
 {
-  pinMode( 12, OUTPUT);
-  pinMode( 13, OUTPUT);
-  pinMode( 14, OUTPUT);
-  pinMode( 27, OUTPUT);
+  pinMode( PWMB, OUTPUT);
+  pinMode( DIRB, OUTPUT);
+  pinMode( DIRA, OUTPUT);
+  pinMode( PWMA, OUTPUT);
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -54,53 +52,61 @@ void setup()
   pinMode(encoder2.PIN, INPUT_PULLUP);
   attachInterrupt(encoder1.PIN, isr, FALLING);
   attachInterrupt(encoder2.PIN, isr2, FALLING);
+  ledcAttachPin(PWMA, 1);
+  ledcAttachPin(PWMB, 2);
+  ledcSetup(1, 12000, 8);
+  ledcSetup(2, 12000, 8);
+  encoder1.numberTicks = 0;
+  encoder2.numberTicks = 0;
   POS = (int*)malloc(sizeof(int) * 2);
   DST = (int*)malloc(sizeof(int) * 2);
-//  ORI = (int*)malloc(sizeof(int));
+  //  ORI = (int*)malloc(sizeof(int));
   if (WiFi.waitForConnectResult() != WL_CONNECTED) {
     Serial.println("WiFi Failed");
     while (1) {
       delay(1000);
     }
   }
-//  listening to both task and current position on this channel
+  //  listening to both task and current position on this channel
   if (udp.listenMulticast(IPAddress(224, 3, 29, 1), 10001)) {
     udp.onPacket([](AsyncUDPPacket packet) {
       deserializeJson(jPos, packet.data());
-      if (STATE == 0){
+      if (STATE == 0) {
         POS[0] = jPos["POS"][0][0];
         POS[1] = jPos["POS"][0][1];
         DST[0] = jPos["Task"][0];
         DST[1] = jPos["Task"][1];
         ORI = jPos["POS"][1];
-//        actionDecoder(POS, DST, ORI);
+        //        int ori = jPos["POS"][1];
+        //        Serial.println(ORI);
+        actionDecoder(POS, DST, ORI);
         Serial.println("Packet received");
       }
     });
   }
-//  robot listening to tasks
-//  if (udp.listenMulticast(IPAddress(224, 3, 29, 2), 10002)){
-//    udp.onPacket([](AsyncUDPPacket packet) {
-//      if (!STATE){
-////        decode the packet at idle STATE
-//        STATE = 1;
-//        DynamicJsonDocument jTask(1024);
-//        deserializeJson(jTask, packet.data());
-//      }
-//    });
-//  }
+  //  robot listening to tasks
+  //  if (udp.listenMulticast(IPAddress(224, 3, 29, 2), 10002)){
+  //    udp.onPacket([](AsyncUDPPacket packet) {
+  //      if (!STATE){
+  ////        decode the packet at idle STATE
+  //        STATE = 1;
+  //        DynamicJsonDocument jTask(1024);
+  //        deserializeJson(jTask, packet.data());
+  //      }
+  //    });
+  //  }
 }
 
-void loop(){
-//  only calculate the path to the destination in STATE 1
-// and the robot knows its current location
-//  if (STATE == 1 && POS[0] >= 0){
-//    int* num = dstSelector(POS, jTask);
-//    int* dst = dstSelector(POS, jTask["Tasks"]);
-//  }
-  if (STATE == 2){
+void loop() {
+  //  only calculate the path to the destination in STATE 1
+  // and the robot knows its current location
+  //  if (STATE == 1 && POS[0] >= 0){
+  //    int* num = dstSelector(POS, jTask);
+  //    int* dst = dstSelector(POS, jTask["Tasks"]);
+  //  }
+  if (STATE == 2) {
     turnLeft(ORI);
-  } else if (STATE == 3){
+  } else if (STATE == 3) {
     turnRight(abs(ORI));
   }
 }
@@ -142,32 +148,38 @@ void jsonHandlerPos(DynamicJsonDocument& jdoc) {
 //  jTask.clear();
 //}
 
-void actionDecoder(int* POS, int* DST, int ORI){
-//  rotate to the right direction
-  if (abs(ORI) >= 5){
-    (ORI >= 0)? turnLeft(ORI):turnRight(abs(ORI));
-//  } else if (abs(DST[1] - POS[1]) >= 1){
-//    int diff = DST[1] - POS[1];
-//    if (diff > 0){
-//      forward(1000);
-//    } else {
-//      turnLeft(180);
-//      forward(abs(diff));
-//    }
-//  } else if (abs(DST[0] - POS[0]) >= 1){
-//    int diff = DST[0] - POS[0];
-//    if (diff > 0){
-//      turnRight(90);
-//      forward(1000);
-//    } else {
-//      turnLeft(90);
-//      forward(1000);
-//    }
+void actionDecoder(int* POS, int* DST, int ORI) {
+  //  rotate to the right direction
+  if (abs(ORI) >= 5) {
+    if (ORI >= 0) {
+      STATE = 2;
+      turnLeft(ORI);
+    } else {
+      STATE = 3;
+      turnRight(abs(ORI));
+    }
   }
+  //  } else if (abs(DST[1] - POS[1]) >= 1){
+  //    int diff = DST[1] - POS[1];
+  //    if (diff > 0){
+  //      forward(1000);
+  //    } else {
+  //      turnLeft(180);
+  //      forward(abs(diff));
+  //    }
+  //  } else if (abs(DST[0] - POS[0]) >= 1){
+  //    int diff = DST[0] - POS[0];
+  //    if (diff > 0){
+  //      turnRight(90);
+  //      forward(1000);
+  //    } else {
+  //      turnLeft(90);
+  //      forward(1000);
+  //    }
 }
 
 void forward(int dist) {
-  if (encoder1.numberTicks < dist){
+  if (encoder1.numberTicks < dist) {
     if ( encoder1.numberTicks > encoder2.numberTicks) {
       motorA (200, 0);
       motorB (0, 0);
@@ -183,22 +195,22 @@ void forward(int dist) {
     motorB(0, 0);
     encoder1.numberTicks = 0;
     encoder2.numberTicks = 0;
-//    STATE = 0;
+    //    STATE = 0;
   }
 }
 
 void turnLeft(int deg) {
-  if (encoder1.numberTicks <= (PULSE / 45) * deg) {
-    if ( encoder1.numberTicks > encoder2.numberTicks) {
-      motorA (100, 0);
-      motorB (0, 0);
-    } else if ( encoder1.numberTicks < encoder2.numberTicks) {
-      motorA (0, 0);
-      motorB (100, 1);
-    } else {
-      motorA(100, 0);
-      motorB(100, 1);
-    }
+  if (encoder1.numberTicks <= PULSE * deg) {
+    //    if ( encoder1.numberTicks > encoder2.numberTicks) {
+    //      motorA (100, 0);
+    //      motorB (0, 0);
+    //    } else if ( encoder1.numberTicks < encoder2.numberTicks) {
+    //      motorA (0, 0);
+    //      motorB (100, 1);
+    //    } else {
+    motorA(100, 0);
+    motorB(100, 1);
+    //    }
   } else {
     motorA(0, 0);
     motorB(0, 0);
@@ -209,24 +221,24 @@ void turnLeft(int deg) {
 }
 
 void turnRight(int deg) {
-  if (encoder1.numberTicks <= (PULSE / 45) * deg) {
-       if ( encoder1.numberTicks > encoder2.numberTicks) {
-        motorA (100, 1);
-        motorB (0, 0);
-       } else if ( encoder1.numberTicks < encoder2.numberTicks){
-        motorA (0, 0);
-        motorB (100, 0);
-      } else {
-        motorA(100, 1);
-        motorB(100, 0);
-      }
-    } else {
-      motorA(0, 0);
-      motorB(0, 0);
-      encoder1.numberTicks = 0;
-      encoder2.numberTicks = 0;
-      STATE = 0;
-    }
+  if (encoder1.numberTicks <= PULSE * deg) {
+    //       if ( encoder1.numberTicks > encoder2.numberTicks) {
+    //        motorA (100, 1);
+    //        motorB (0, 0);
+    //       } else if ( encoder1.numberTicks < encoder2.numberTicks){
+    //        motorA (0, 0);
+    //        motorB (100, 0);
+    //      } else {
+    motorA(100, 1);
+    motorB(100, 0);
+    //      }
+  } else {
+    motorA(0, 0);
+    motorB(0, 0);
+    encoder1.numberTicks = 0;
+    encoder2.numberTicks = 0;
+    STATE = 0;
+  }
 }
 
 void motorA ( int speed, int direction) {
