26659edc16dd458e3347ed8196c83d79e408422c
nh8157 <47706495+nh8157@users.noreply.github.com>
2020-07-09 20:34:03 +0800

    collision avoidance file created

---
+++ SOFT/testCollisionAvoidance/testCollisionAvoidance.ino
@@ -0,0 +1,130 @@
+class Receiver
+{
+private:
+    int _idx;
+    int _pin_num;
+    int _reading;
+
+public:
+    Receiver(int pin_num, int _idx);
+    int get_idx();
+    int get_pin();
+    int get_reading();
+    void receiver_init();
+    void receiver_read();
+}
+
+Receiver::Receiver(int pin_num, int idx)
+{
+    _idx = idx;
+    _pin_num = pin_num;
+}
+
+int Receiver::get_idx()
+{
+    return _idx;
+}
+
+int Receiver::get_pin()
+{
+    return _idx;
+}
+
+int Receiver::get_reading()
+{
+    return _reading;
+}
+
+void Receiver::receiver_init()
+{
+    analogSetPinAttenuation(get_pin(), ADC_11db);
+}
+
+void Receiver::receiver_read()
+{
+    _reading = analogRead(get_pin());
+}
+
+class Emitter
+{
+private:
+    int _em_tx;
+    int _on_interval;
+    int _off_interval;
+    int _emitter_state;
+    float _emitter_timer;
+
+public:
+    Emitter(int em_tx);
+    void emitter_on();
+    void emitter_off();
+    void emitter_init();
+    void emitter_control();
+}
+
+Emitter::Emitter(int em_tx)
+{
+    _em_tx = em_tx;
+    _on_interval = 1;
+    _off_interval = 20;
+    _emitter_state = 0;
+}
+
+void Emitter::emitter_init()
+{
+    pinMode(_em_tx, OUTPUT);
+    _emitter_timer = millis();
+}
+
+void Emitter::emitter_on()
+{
+    digitalWrite(_em_tx, HIGH);
+}
+
+void Emitter::emitter_off()
+{
+    digitalWrite(_em_tx, LOW);
+}
+
+void Emitter::emitter_control()
+{
+    if (_emitter_state) // emitter is turned on
+    {
+        float current = millis();
+        if (current - _emitter_timer > _on_interval)
+        {
+            emitter_off();
+            _emitter_timer = millis();
+            _emitter_state = 0;
+        }
+    }
+    else
+    {
+        float current = millis();
+        if (current - _emitter_timer > _off_interval)
+        {
+            emitter_on();
+            _emitter_timer = millis();
+            _emitter_state = 1;
+        }
+    }
+
+    class ColAvo
+    {
+    private:
+        Emitter _emitter;
+        Receiver _receivers[5];
+
+    public:
+        ColAvo();
+        void collision_avoidance_init();
+    }
+
+    ColAvo::ColAvo() : _emitter(23)
+    {
+        _receivers[0] = Receiver(32, 0);
+        _receivers[1] = Receiver(33, 1);
+        _receivers[2] = Receiver(34, 2);
+        _receivers[3] = Receiver(36, 3);
+        _receivers[4] = Receiver(39, 4);
+    }
